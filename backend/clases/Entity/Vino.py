729ca82daee7b2a9a3e0aa1e9f4fb5ffb243d1a9
ferrer86827 <86827@sistemas.frc.utn.edu.ar>
2024-05-29 20:10:31 -0300

    Nuevos casos de prueba con periodo

--- backend/clases/Entity/Vino.py
+++ backend/clases/Entity/Vino.py
@@ -80,8 +80,8 @@ vino1 = Vino(
 vino2 = Vino(
     2019, "img_etiqueta_2.jpg", "Merlot", "Aromas a frutas negras y especias", 1300.00,
     [
-        Resenia("Sabor suave y aterciopelado", False, '2020-09-12', 8, None),
-        Resenia("No me gustó", False, '2020-11-25', 6, None)
+        Resenia("Sabor suave y aterciopelado", False, '2023-02-12', 8, None),
+        Resenia("No me gustó", False, '2023-02-25', 6, None)
     ],
     Varietal("Toques de roble", 70),
     Bodega('2018-07-20', "Bodega Catena Zapata", "Historia de Catena Zapata", "Descripción detallada de Catena Zapata",
@@ -146,7 +146,7 @@ vino7 = Vino(
 vino8 = Vino(
     2018, "img_etiqueta_8.jpg", "Sauvignon Blanc", "Notas de hierbas frescas", 1350.00,
     [
-        Resenia("Fresco y mineral", True, '2019-11-12', 9, None)
+        Resenia("Fresco y mineral", True, '2023-01-23', 9, None)
     ],
     Varietal("Aromas herbales", 80),
     Bodega('2018-05-05', "Bodega Amalaya", "Historia de Amalaya", "Descripción detallada de Amalaya",
@@ -166,7 +166,7 @@ vino9 = Vino(
 vino10 = Vino(
     2021, "img_etiqueta_10.jpg", "Bonarda", "Aromas a frutas maduras", 1500.00,
     [
-        Resenia("Gran relación calidad-precio", True, '2024-05-18', 9, None)
+        Resenia("Gran relación calidad-precio", True, '2023-02-18', 9, None)
     ],
     Varietal("Aromas a frutas rojas", 85),
     Bodega('2018-12-12', "Bodega La Rural", "Historia de La Rural", "Descripción detallada de La Rural",
@@ -176,129 +176,129 @@ vino10 = Vino(
 vino11 = Vino(
     2022, "img_etiqueta_11.jpg", "Viognier", "Aromas a durazno y albaricoque", 1700.00, 
     [
-        Resenia("Excelente para mariscos", True, '10-05-2023', 9, None),
-        Resenia("Muy floral", True, '12-06-2023', 8, None)
+        Resenia("Excelente para mariscos", True, '2023-02-10', 9, None),
+        Resenia("Muy floral", True, '2023-06-12', 8, None)
     ], 
     Varietal("Aromas frutales y florales", 85), 
-    Bodega('10-10-2019', "Bodega Familia Schroeder", "Historia de Familia Schroeder", "Descripción detallada de Familia Schroeder", 
+    Bodega('2019-10-10', "Bodega Familia Schroeder", "Historia de Familia Schroeder", "Descripción detallada de Familia Schroeder", 
            RegionVitivinicola("Región vitivinícola de Patagonia", "Patagonia", Provincia("Neuquén", Pais("Argentina"))))
 )
 
 vino12 = Vino(
     2021, "img_etiqueta_12.jpg", "Petit Verdot", "Notas de frutos negros y violetas", 1600.00, 
     [
-        Resenia("Muy buen cuerpo", True, '18-07-2022', 9, None),
-        Resenia("Un vino con carácter", True, '22-08-2022', 8, None)
+        Resenia("Muy buen cuerpo", True, '2022-07-18', 9, None),
+        Resenia("Un vino con carácter", True, '2022-08-22', 8, None)
     ], 
     Varietal("Aromas a frutos oscuros", 80), 
-    Bodega('05-04-2020', "Bodega Pulenta Estate", "Historia de Pulenta Estate", "Descripción detallada de Pulenta Estate", 
+    Bodega('2020-04-05', "Bodega Pulenta Estate", "Historia de Pulenta Estate", "Descripción detallada de Pulenta Estate", 
            RegionVitivinicola("Región vitivinícola de Mendoza", "Mendoza", Provincia("Mendoza", Pais("Argentina"))))
 )
 
 vino13 = Vino(
     2019, "img_etiqueta_13.jpg", "Cabernet Franc", "Aromas a pimiento y especias", 1500.00, 
     [
-        Resenia("Perfecto para platos especiados", False, '12-10-2020', 8, None),
-        Resenia("Sabor fuerte", False, '15-11-2020', 7, None)
+        Resenia("Perfecto para platos especiados", False, '2020-10-12', 8, None),
+        Resenia("Sabor fuerte", False, '2020-11-15', 7, None)
     ], 
     Varietal("Aromas especiados", 70), 
-    Bodega('20-06-2018', "Bodega Doña Paula", "Historia de Doña Paula", "Descripción detallada de Doña Paula", 
+    Bodega('2018-06-20', "Bodega Doña Paula", "Historia de Doña Paula", "Descripción detallada de Doña Paula", 
            RegionVitivinicola("Región vitivinícola de San Juan", "San Juan", Provincia("San Juan", Pais("Argentina"))))
 )
 
 vino14 = Vino(
     2020, "img_etiqueta_14.jpg", "Grenache", "Aromas a frambuesas y fresas", 1450.00, 
     [
-        Resenia("Muy afrutado", True, '05-02-2021', 9, None),
-        Resenia("Ideal para días calurosos", True, '10-03-2021', 8, None)
+        Resenia("Muy afrutado", True, '2023-02-01', 9, None),
+        Resenia("Ideal para días calurosos", True, '2023-02-10', 8, None)
     ], 
     Varietal("Aromas frutales intensos", 80), 
-    Bodega('25-11-2017', "Bodega El Porvenir", "Historia de El Porvenir", "Descripción detallada de El Porvenir", 
+    Bodega('2017-11-15', "Bodega El Porvenir", "Historia de El Porvenir", "Descripción detallada de El Porvenir", 
            RegionVitivinicola("Región vitivinícola de Salta", "Salta", Provincia("Salta", Pais("Argentina"))))
 )
 
 vino15 = Vino(
     2018, "img_etiqueta_15.jpg", "Zinfandel", "Notas de moras y vainilla", 1500.00, 
     [
-        Resenia("Muy suave y equilibrado", True, '22-08-2019', 9, None),
-        Resenia("Un poco dulce", True, '30-09-2019', 8, None),
-        Resenia("Perfecto para una cena especial", True, '05-12-2019', 9, None),
-        Resenia("Excelente relación calidad-precio", True, '10-01-2020', 9, None)
+        Resenia("Muy suave y equilibrado", True, '2019-08-04', 9, None),
+        Resenia("Un poco dulce", True, '2019-09-14', 8, None),
+        Resenia("Perfecto para una cena especial", True, '2023-02-02', 9, None),
+        Resenia("Excelente relación calidad-precio", True, '2023-01-25', 9, None)
     ], 
     Varietal("Aromas a frutas maduras", 75), 
-    Bodega('05-03-2016', "Bodega Humberto Canale", "Historia de Humberto Canale", "Descripción detallada de Humberto Canale", 
+    Bodega('2016-03-20', "Bodega Humberto Canale", "Historia de Humberto Canale", "Descripción detallada de Humberto Canale", 
            RegionVitivinicola("Región vitivinícola de Patagonia", "Patagonia", Provincia("Río Negro", Pais("Argentina"))))
 )
 
 vino16 = Vino(
     2021, "img_etiqueta_16.jpg", "Sangiovese", "Aromas a cerezas y tabaco", 1600.00, 
     [
-        Resenia("Perfecto con pasta", True, '10-05-2022', 9, None),
-        Resenia("Gran estructura", True, '15-06-2022', 8, None),
-        Resenia("Muy buen vino para su precio", True, '10-09-2022', 9, None),
-        Resenia("Aromas intensos y complejos", True, '20-10-2022', 8, None)
+        Resenia("Perfecto con pasta", True, '2023-01-12', 9, None),
+        Resenia("Gran estructura", True, '2023-01-27', 8, None),
+        Resenia("Muy buen vino para su precio", True, '2023-02-18', 9, None),
+        Resenia("Aromas intensos y complejos", True, '2023-02-20', 8, None)
     ], 
     Varietal("Aromas frutales y especiados", 85), 
-    Bodega('10-07-2019', "Bodega Navarro Correas", "Historia de Navarro Correas", "Descripción detallada de Navarro Correas", 
+    Bodega('2002-07-19', "Bodega Navarro Correas", "Historia de Navarro Correas", "Descripción detallada de Navarro Correas", 
            RegionVitivinicola("Región vitivinícola de Mendoza", "Mendoza", Provincia("Mendoza", Pais("Argentina"))))
 )
 
 vino17 = Vino(
     2020, "img_etiqueta_17.jpg", "Riesling", "Aromas a manzana verde y miel", 1400.00, 
     [
-        Resenia("Muy fresco y vibrante", True, '12-04-2021', 9, None),
-        Resenia("Un poco dulce para mi gusto", True, '18-05-2021', 8, None),
-        Resenia("Ideal para acompañar postres", True, '05-08-2021', 8, None),
-        Resenia("Excelente para una tarde de verano", True, '20-09-2021', 9, None)
+        Resenia("Muy fresco y vibrante", True, '2021-04-15', 9, None),
+        Resenia("Un poco dulce para mi gusto", True, '2023-01-20', 8, None),
+        Resenia("Ideal para acompañar postres", True, '2023-02-13', 8, None),
+        Resenia("Excelente para una tarde de verano", True, '2021-09-02', 9, None)
     ], 
     Varietal("Aromas florales y frutales", 80), 
-    Bodega('15-01-2018', "Bodega Luigi Bosca", "Historia de Luigi Bosca", "Descripción detallada de Luigi Bosca", 
+    Bodega('2018-01-15', "Bodega Luigi Bosca", "Historia de Luigi Bosca", "Descripción detallada de Luigi Bosca", 
            RegionVitivinicola("Región vitivinícola de La Rioja", "La Rioja", Provincia("La Rioja", Pais("Argentina"))))
 )
 
 vino18 = Vino(
     2022, "img_etiqueta_18.jpg", "Moscato", "Aromas a flores blancas y cítricos", 1200.00, 
     [
-        Resenia("Muy dulce y aromático", True, '22-03-2023', 8, None),
-        Resenia("Perfecto para postres", True, '10-04-2023', 9, None),
-        Resenia("Ideal para una tarde de verano", True, '05-06-2023', 8, None),
-        Resenia("Excelente para una fiesta", True, '12-07-2023', 9, None)
+        Resenia("Muy dulce y aromático", True, '2023-02-23', 8, None),
+        Resenia("Perfecto para postres", True, '2023-01-22', 9, None),
+        Resenia("Ideal para una tarde de verano", True, '2023-05-23', 8, None),
+        Resenia("Excelente para una fiesta", True, '2023-07-10', 9, None)
     ], 
     Varietal("Aromas frutales y florales", 75), 
-    Bodega('18-08-2020', "Bodega Santa Julia", "Historia de Santa Julia", "Descripción detallada de Santa Julia", 
+    Bodega('2020-08-15', "Bodega Santa Julia", "Historia de Santa Julia", "Descripción detallada de Santa Julia", 
            RegionVitivinicola("Región vitivinícola de Mendoza", "Mendoza", Provincia("Mendoza", Pais("Argentina"))))
 )
 
 vino19 = Vino(
     2020, "img_etiqueta_19.jpg", "Rosado", "Aromas a fresas y melón", 1100.00, 
     [
-        Resenia("Muy fresco y afrutado", False, '05-11-2021', 8, None),
-        Resenia("Ideal para verano", False, '12-12-2021', 7, None)
+        Resenia("Muy fresco y afrutado", False, '2023-02-21', 8, None),
+        Resenia("Ideal para verano", False, '2023-12-10', 7, None)
     ], 
     Varietal("Aromas a frutas rojas", 65), 
-    Bodega('15-10-2019', "Bodega Séptima", "Historia de Séptima", "Descripción detallada de Séptima", 
+    Bodega('2019-10-20', "Bodega Séptima", "Historia de Séptima", "Descripción detallada de Séptima", 
            RegionVitivinicola("Región vitivinícola de Mendoza", "Mendoza", Provincia("Mendoza", Pais("Argentina"))))
 )
 
 vino20 = Vino(
     2021, "img_etiqueta_20.jpg", "Garnacha", "Notas de frambuesas y hierbas", 1350.00, 
     [
-        Resenia("Muy afrutado y fresco", True, '10-06-2022', 9, None),
-        Resenia("Sabor complejo", True, '15-07-2022', 8, None)
+        Resenia("Muy afrutado y fresco", True, '2023-01-31', 9, None),
+        Resenia("Sabor complejo", True, '2023-02-05', 8, None)
     ], 
     Varietal("Aromas frutales y herbales", 80), 
-    Bodega('25-02-2020', "Bodega Finca Las Moras", "Historia de Finca Las Moras", "Descripción detallada de Finca Las Moras", 
+    Bodega('2021-02-20', "Bodega Finca Las Moras", "Historia de Finca Las Moras", "Descripción detallada de Finca Las Moras", 
            RegionVitivinicola("Región vitivinícola de San Juan", "San Juan", Provincia("San Juan", Pais("Argentina"))))
 )
 
 vino21 = Vino(
     2018, "img_etiqueta_21.jpg", "Carmenere", "Aromas a frutas negras y especias", 1500.00, 
     [
-        Resenia("Muy especiado y complejo", True, '05-09-2019', 9, None),
-        Resenia("Buen cuerpo", True, '12-10-2019', 8, None)
+        Resenia("Muy especiado y complejo", True, '2019-09-02', 9, None),
+        Resenia("Buen cuerpo", True, '2019-10-19', 8, None)
     ], 
     Varietal("Aromas a frutas maduras", 80), 
-    Bodega('10-11-2017', "Bodega Alta Vista", "Historia de Alta Vista", "Descripción detallada de Alta Vista", 
+    Bodega('2017-11-20', "Bodega Alta Vista", "Historia de Alta Vista", "Descripción detallada de Alta Vista", 
            RegionVitivinicola("Región vitivinícola de Mendoza", "Mendoza", Provincia("Mendoza", Pais("Argentina"))))
 )
 
